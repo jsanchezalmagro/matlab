cd0d86238e53cb1b5de52f0eb621ecd386b0df64
jsanchez <jsanchez@gmail.com>
2023-03-05 11:39:38 +0100

    New reference to path

--- sim/seq001/seq.m
+++ sim/seq001/seq.m
@@ -1,21 +1,20 @@
-% PATH define
+% Define Python modules by USER 
+modules = ["diff_enc", "diff_dec"];
+
+% PATH define, not modife--------------------------------------------------
 currentFolder = pwd;
 modelFolder = fullfile(currentFolder,"..","..");
-
 % Add to MATLAB path:
 addpath(currentFolder);
-
-% Add to Python path:
-folders = [ fullfile(modelFolder, 'diff_enc'), ...
-            fullfile(modelFolder, 'diff_dec')];
-
 % Add to Python path to ps.sys.path:
 P = py.sys.path;
-for folder = folders
+for module = modules
+    folder = fullfile(modelFolder, module);
     if count(P,folder) == 0
         insert(P,int32(0),folder);
     end
 end
+%--------------------------------------------------------------------------
 
 % Load Python module
 %clear classes
@@ -24,6 +23,7 @@ M_diff_dec = py.importlib.import_module('diff_dec');
 
 py.importlib.reload(M_diff_enc);
 py.importlib.reload(M_diff_dec);
+
 diff_enc_int = M_diff_enc.Diff_enc.feval(2.0);
 diff_dec_int = M_diff_dec.Diff_dec.feval(3.9);
 
@@ -40,3 +40,4 @@ dec = double(M_diff_dec.Diff_dec.diff_dec('m', pyvar))
 
 %diff_dec_int.base
 %diff_enc_int.base
+
